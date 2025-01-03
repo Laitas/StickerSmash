f3dc0eb83ae93ee466fcfa75d72f216ed902281f
Laitas <61273117+Laitas@users.noreply.github.com>
2025-01-03 18:07:59 +0200

    Upload image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,41 @@
 import PlaceholderImage from "@/assets/images/background-image.png";
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<undefined | string>(
+    undefined
+  );
+
+  const pickImageAsync = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      1;
+      alert("You did not select any image.");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer
+          imgSource={selectedImage ? { uri: selectedImage } : PlaceholderImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary">Choose a photo</Button>
+        <Button theme="primary" onPress={pickImageAsync}>
+          Choose a photo
+        </Button>
         <Button>Use this photo</Button>
       </View>
     </View>
